4ead206aba0b6467acfe8292b23ac2e9b29d56e3
Bruno Luz <bsilva.se@gmail.com>
2021-02-01 21:14:51 -0300

    main - Configurando build Docker com o Maven (ms-usuarios)

--- dockerk8s-ms-receitas/src/main/java/dev/bstk/dockerk8s/msreceitas/Application.java
+++ dockerk8s-ms-receitas/src/main/java/dev/bstk/dockerk8s/msreceitas/Application.java
@@ -1,12 +1,19 @@
 package dev.bstk.dockerk8s.msreceitas;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+
     public static void main(String[] args) {
+        LOGGER.info("*******************************");
+        LOGGER.info("***    MS-RECEITAS >> OK    ***");
+        LOGGER.info("*******************************");
         SpringApplication.run(Application.class, args);
     }
 
