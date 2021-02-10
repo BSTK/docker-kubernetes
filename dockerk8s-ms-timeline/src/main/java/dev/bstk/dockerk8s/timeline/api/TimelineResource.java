b28d4ece7c0a5a4b4ac648c9c2533dea50b56d78
Bruno Luz <bsilva.se@gmail.com>
2021-02-10 19:12:34 -0300

    Adicionando logs

--- dockerk8s-ms-timeline/src/main/java/dev/bstk/dockerk8s/timeline/api/TimelineResource.java
+++ dockerk8s-ms-timeline/src/main/java/dev/bstk/dockerk8s/timeline/api/TimelineResource.java
@@ -1,6 +1,7 @@
 package dev.bstk.dockerk8s.timeline.api;
 
 import dev.bstk.dockerk8s.timeline.domain.TimelineRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,8 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Slf4j
 @RestController
-@RequestMapping("/api/v1/timeline")
+@RequestMapping("/api/v1/timelines")
 public class TimelineResource {
 
     private final ModelMapper mapper;
@@ -25,12 +27,13 @@ public class TimelineResource {
     }
 
     @RequestMapping
-    public ResponseEntity<List<TimelineResponse>> usuarios() {
+    public ResponseEntity<List<TimelineResponse>> timelines() {
         final var timelines = repository.timelines();
         final var timelinesResponse = timelines.stream()
             .map(timeline -> mapper.map(timeline, TimelineResponse.class))
             .collect(Collectors.toList());
 
+        log.warn("Request endpoint: /timelines");
         return ResponseEntity.ok(timelinesResponse);
     }
 
